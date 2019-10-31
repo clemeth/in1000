343b146f5c59c6a853d4b2a7c8f0879239f922c3
Clemeth <daniel@bjerkeland.com>
2019-10-31 12:39:00 +0100

    Legg til testprogram for kodestafett uke 11

---
+++ Uke 11/test_kalkulator.py
@@ -0,0 +1,10 @@
+from kalkulator import Kalkulator
+
+kalk = Kalkulator()
+
+print(kalk.kalkuler("1 + 2"))
+print(kalk.kalkuler("123 - 17"))
+print(kalk.kalkuler("50 / 2"))
+print(kalk.kalkuler("15 * 3"))
+print(kalk.kalkuler("3 ^ 5"))
+print(kalk.kalkuler("3 / 0"))
