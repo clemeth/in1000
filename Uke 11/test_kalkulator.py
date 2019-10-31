cd6d6ca547e7075599651eafd76cb34878a860e2
Clemeth <daniel@bjerkeland.com>
2019-10-31 13:05:36 +0100

    Legg til kommentarer i testprogram

--- Uke 11/test_kalkulator.py
+++ Uke 11/test_kalkulator.py
@@ -2,9 +2,9 @@ from kalkulator import Kalkulator
 
 kalk = Kalkulator()
 
-print(kalk.kalkuler("1 + 2"))
-print(kalk.kalkuler("123 - 17"))
-print(kalk.kalkuler("50 / 2"))
-print(kalk.kalkuler("15 * 3"))
-print(kalk.kalkuler("3 ^ 5"))
-print(kalk.kalkuler("3 / 0"))
+print(kalk.kalkuler("1 + 2")) # 3
+print(kalk.kalkuler("123 - 17")) # 106
+print(kalk.kalkuler("50 / 2")) # 25.0
+print(kalk.kalkuler("15 * 3")) # 45
+print(kalk.kalkuler("3 ^ 5")) # 243
+print(kalk.kalkuler("3 / 0")) # Nulldivisjon
